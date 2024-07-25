df3c84a8d212f36fbc465c47bda0fa8d750db7fa
Alberto Ricart <alberto@synadia.com>
2024-07-25 17:31:57 -0500

    [cli] added script to sync bundle versions between the different modules

--- bin/check-dep-versions.ts
+++ bin/check-dep-versions.ts
@@ -14,103 +14,308 @@
  * limitations under the License.
  */
 
-import { parseArgs } from "jsr:@std/cli/parse-args";
+// this command checks that bundles dependencies have a minimum floor dependency
+// matching the current version of a bundle. This way the next release
+// raises the minimum version of the dependency to currently released versions
+// of the dependency.
+
 import { join } from "jsr:@std/path";
+import { loadPackageFile, SemVer } from "./lib/bundle_versions.ts";
 
-async function load<T>(fp: string): Promise<T> {
-  const src = await Deno.readTextFile(fp);
-  return JSON.parse(src);
-}
-
-type denoImports = {
+type PackageJSON = {
+  name: string;
+  version: string;
+  dependencies: Record<string, string>;
+};
+type DenoJSON = {
+  name: string;
+  version: string;
+  imports: Record<string, string>;
+};
+type Imports = {
   imports: Record<string, string>;
 };
 
-type nodeDependencies = {
-  dependencies: Record<string, string>;
-};
+class ImportMap {
+  data: Imports;
+  changed: boolean;
 
-const argv = parseArgs(
-  Deno.args,
-  {
-    alias: {
-      "m": ["module"],
-      "v": ["verbose"],
-      "f": ["fix"],
-    },
-    string: ["module"],
-    boolean: ["v", "f"],
-  },
-);
-
-const module = argv.module || null;
-
-if (module === null) {
-  console.error(
-    `[ERROR] --module is required`,
-  );
-  Deno.exit(1);
-}
-
-const di = await load<denoImports>(join(module, "deno.json"));
-const nd = await load<nodeDependencies>(join(module, "package.json"));
-
-// drive the dependencies from the deno.json, as npm may contain additional ones
-let failed = false;
-let fixed = false;
-
-for (const lib in di.imports) {
-  let deno = di.imports[lib];
-  const prefix = `jsr:${lib}@`;
-  if (deno.startsWith(prefix)) {
-    deno = deno.substring(prefix.length);
+  constructor(data: Imports) {
+    this.data = data;
+    this.changed = false;
   }
+  static async load(dir: string): Promise<ImportMap | null> {
+    const data = await loadPackageFile<Imports>(
+      join(dir, "import_map.json"),
+    );
 
-  if (argv.f) {
-    if (nd.dependencies[lib] !== deno) {
-      console.log(
-        `changed in package.json ${lib} from ${
-          nd.dependencies[lib]
-        } to ${deno}`,
-      );
-      nd.dependencies[lib] = deno;
-      fixed = true;
+    if (data.imports) {
+      return new ImportMap(data);
     }
-    continue;
+
+    return null;
   }
 
-  if (argv.v === true) {
-    const node = nd.dependencies[lib];
-    console.log({ lib, deno, node });
+  has(module: string): SemVer | null {
+    if (this.data.imports) {
+      const v = this.data.imports[module];
+      // we only update them when they have a jsr - otherwise it is local file
+      if (v?.startsWith("jsr:")) {
+        return DenoModule.parseVersion(
+          this.data.imports[module],
+        );
+      }
+    }
+    return null;
   }
-  if (!nd.dependencies[lib]) {
-    failed = true;
-    console.log(
-      `[ERROR] module ${module} package.json dependencies is missing: ${lib}`,
+
+  update(module: string, version: SemVer): boolean {
+    let changed = false;
+    if (this.data.imports) {
+      const have = this.has(module);
+      if (have && version.compare(have) !== 0) {
+        this.data.imports[module] = `jsr:${module}@~${version.string()}`;
+        changed = true;
+      }
+      const internalModule = `${module}/internal`;
+      const haveInternal = this.has(internalModule);
+      if (haveInternal && version.compare(haveInternal) !== 0) {
+        this.data.imports[internalModule] =
+          `jsr:${module}@~${version.string()}/internal`;
+        changed = true;
+      }
+      return changed;
+    }
+    return false;
+  }
+
+  store(dir: string): Promise<void> {
+    this.changed = false;
+    return Deno.writeTextFile(
+      join(dir, "import_map.json"),
+      JSON.stringify(this.data, null, 2),
     );
-    continue;
   }
-  if (deno !== nd.dependencies[lib]) {
-    failed = true;
-    console.log(
-      `[ERROR] module ${module} package.json dependencies ${lib}: ${
-        nd.dependencies[lib]
-      } - but should be ${deno}`,
+}
+
+class BaseModule {
+  name: string;
+  version: SemVer;
+
+  constructor(name: string, version: SemVer) {
+    this.name = name;
+    this.version = version;
+  }
+}
+
+class DenoModule extends BaseModule {
+  data: DenoJSON;
+  changed: boolean;
+
+  constructor(name: string, version: SemVer, data: DenoJSON) {
+    super(name, version);
+    this.data = data;
+    this.changed = false;
+  }
+
+  static async load(dir: string): Promise<DenoModule | null> {
+    const data = await loadPackageFile<DenoJSON>(
+      join(dir, "deno.json"),
+    );
+
+    if (data.version) {
+      return new DenoModule(data.name, new SemVer(data.version), data);
+    }
+
+    return null;
+  }
+
+  store(dir: string): Promise<void> {
+    this.changed = false;
+    return Deno.writeTextFile(
+      join(dir, "deno.json"),
+      JSON.stringify(this.data, null, 2),
+    );
+  }
+
+  has(module: string): SemVer | null {
+    if (this.data.imports) {
+      return DenoModule.parseVersion(
+        this.data.imports[module],
+      );
+    }
+    return null;
+  }
+
+  update(module: string, version: SemVer): boolean {
+    if (this.data.imports) {
+      const have = this.has(module);
+      if (have && version.compare(have) !== 0) {
+        this.data.imports[module] = `jsr:${module}@~${version.string()}`;
+        this.changed = true;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static parseVersion(v: string): SemVer | null {
+    // jsr:@nats-io/something@[^|~]0.0.0-something
+    if (v) {
+      v = v.substring(v.lastIndexOf("@"));
+      return v.startsWith("^") || v.startsWith("~")
+        ? new SemVer(v.substring(1))
+        : new SemVer(v);
+    }
+    return null;
+  }
+}
+
+class NodeModule extends BaseModule {
+  data: PackageJSON;
+  changed: boolean;
+
+  constructor(name: string, version: SemVer) {
+    super(name, version);
+    this.data = {} as PackageJSON;
+    this.changed = false;
+  }
+
+  static async load(dir: string): Promise<NodeModule | null> {
+    const packageJSON = await loadPackageFile<PackageJSON>(
+      join(dir, "package.json"),
+    );
+
+    if (packageJSON.version) {
+      const m = new NodeModule(
+        packageJSON.name,
+        new SemVer(packageJSON.version),
+      );
+      m.data = packageJSON;
+      return m;
+    }
+
+    return null;
+  }
+
+  has(module: string): SemVer | null {
+    if (this.data.dependencies) {
+      return NodeModule.parseVersion(
+        this.data.dependencies[module],
+      );
+    }
+    return null;
+  }
+
+  static parseVersion(v: string): SemVer | null {
+    if (v) {
+      return v.startsWith("^") || v.startsWith("~")
+        ? new SemVer(v.substring(1))
+        : new SemVer(v);
+    }
+    return null;
+  }
+
+  update(module: string, version: SemVer): boolean {
+    if (this.data.dependencies) {
+      const have = this.has(module);
+      if (have && version.compare(have) !== 0) {
+        this.data.dependencies[module] = `~${version.string()}`;
+        this.changed = true;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  store(dir: string): Promise<void> {
+    this.changed = false;
+    return Deno.writeTextFile(
+      join(dir, "package.json"),
+      JSON.stringify(this.data, null, 2),
     );
   }
 }
 
-if (argv.f && fixed) {
-  await Deno.writeTextFile(
-    join(module, "package.json"),
-    JSON.stringify(nd, undefined, " "),
-  );
-  console.log(`[OK] module ${module} updated package.json`);
-  Deno.exit(0);
+const dirs = [
+  "core",
+  "services",
+  "jetstream",
+  "kv",
+  "obj",
+  "transport-deno",
+  "transport-node",
+];
+
+let nuid = new SemVer("0.0.0");
+let nkeys = new SemVer("0.0.0");
+
+for (const dir of dirs) {
+  const dm = await DenoModule.load(dir);
+  const nm = await NodeModule.load(dir);
+
+  if (dm && nm) {
+    if (dm.version.compare(nm.version) !== 0) {
+      throw new Error(
+        `expected package.json and deno.json to match for ${dir}`,
+      );
+    }
+  }
+
+  const v = dm ? dm.version : nm!.version;
+  const moduleName = dm ? dm.name : nm!.name;
+
+  const other = dirs.filter((m) => {
+    return m !== dir;
+  });
+
+  for (const d of other) {
+    const dmm = await DenoModule.load(d);
+    if (dmm) {
+      if (dmm.has(moduleName)) {
+        dmm.update(moduleName, v);
+        await dmm.store(d);
+      }
+    }
+    const nmm = await NodeModule.load(d);
+    if (nmm) {
+      if (nmm.has(moduleName)) {
+        nmm.update(moduleName, v);
+        await nmm.store(d);
+      }
+      const onuid = nmm.has("@nats-io/nuid");
+      if (onuid) {
+        nuid = nuid.max(onuid);
+      }
+      const onkeys = nmm.has("@nats-io/nkeys");
+      if (onkeys) {
+        nkeys = nkeys.max(onkeys);
+      }
+    }
+
+    const map = await ImportMap.load(d);
+    if (map) {
+      if (map.has(moduleName)) {
+        map.update(moduleName, v);
+        await map.store(d);
+      }
+    }
+  }
 }
 
-if (failed) {
-  Deno.exit(1);
+for (const d of dirs) {
+  const dmm = await DenoModule.load(d);
+  if (dmm) {
+    if (dmm.has("@nats-io/nuid")) {
+      dmm.update("@nats-io/nuid", nuid);
+      await dmm.store(d);
+    }
+  }
+  const nmm = await NodeModule.load(d);
+  if (nmm) {
+    if (nmm.has("@nats-io/nkeys")) {
+      nmm.update("@nats-io/nkeys", nkeys);
+      await nmm.store(d);
+    }
+  }
 }
-console.log(`[OK] module ${module}`);
-Deno.exit(0);
