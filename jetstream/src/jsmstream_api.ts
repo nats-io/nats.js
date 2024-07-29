5d50fabf40fb64e0aeee2ff0fc4da953de40171d
Alberto Ricart <alberto@synadia.com>
2024-07-29 18:03:50 -0500

    fix(jetstream): fixed possible dereference of undefined value when validating stream sources
    fix(kv): fixed handling of kv sources by adding a necessary transform
    
    --
    This fix was ported from https://github.com/nats-io/nats.deno/pull/721

--- jetstream/src/jsmstream_api.ts
+++ jetstream/src/jsmstream_api.ts
@@ -255,7 +255,7 @@ export class StreamAPIImpl extends BaseApiClientImpl implements StreamAPI {
       context: string,
       src: Partial<StreamSource>,
     ): void {
-      const count = src.subject_transforms?.length || 0;
+      const count = src?.subject_transforms?.length || 0;
       if (count > 0) {
         const { min, ok } = nci.features.get(
           Feature.JS_STREAM_SOURCE_SUBJECT_TRANSFORM,
