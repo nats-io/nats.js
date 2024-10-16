760d3d2a02f4305b74db04acf024e4e0ee56a07f
Alberto Ricart <alberto@synadia.com>
2024-10-15 19:11:32 -0500

    tests(core): stats reporting
    Signed-off-by: Alberto Ricart <alberto@synadia.com>

--- test_helpers/launcher.ts
+++ test_helpers/launcher.ts
@@ -89,6 +89,8 @@ export interface Conn {
   "pending_bytes": number;
   "in_msgs": number;
   "out_msgs": number;
+  "in_bytes": number;
+  "out_bytes": number;
   subscriptions: number;
   name: string;
   lang: string;
