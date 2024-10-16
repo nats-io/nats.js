6771b0b3cba6ede1dff84b3319bb7d2f39e2839d
Alberto Ricart <alberto@synadia.com>
2024-10-16 11:15:10 -0500

    fix(obj): Re-exported StorageType and Placement from objectstore module (#79)
    This commit enhances the @nats-io/obj module by re-exporting the `StorageType` and `Placement` types from the "@nats-io/jetstream" library. This change ensures that these types are available for use in applications that use objectstore.
    
    Signed-off-by: Alberto Ricart <alberto@synadia.com>

--- obj/src/mod.ts
+++ obj/src/mod.ts
@@ -8,6 +8,9 @@ export type {
   ObjectStoreOptions,
   ObjectStorePutOpts,
   ObjectStoreStatus,
+  Placement,
 } from "./internal_mod.ts";
+
+export { StorageType } from "./internal_mod.ts";
 
 export { Objm } from "./objectstore.ts";
