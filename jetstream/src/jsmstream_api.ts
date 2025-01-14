f78c0c23a02318aae6324f2ef80b4172c9ea16d6
Alberto Ricart <alberto@synadia.com>
2025-01-14 14:27:06 -0600

    feat(JS): `getConsumerFromInfo` method for pull consumer retrieval
    Introduce a `getConsumerFromInfo` method to retrieve a pull consumer without fetching ConsumerInfo. This ensures efficient consumer handling while avoiding unnecessary API calls. Included corresponding tests to validate functionality.
    
    Signed-off-by: Alberto Ricart <alberto@synadia.com>

--- jetstream/src/jsmstream_api.ts
+++ jetstream/src/jsmstream_api.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 The NATS Authors
+ * Copyright 2021-2025 The NATS Authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -242,6 +242,13 @@ export class ConsumersImpl implements Consumers {
     } else {
       return this.ordered(stream, name);
     }
+  }
+
+  getConsumerFromInfo(ci: ConsumerInfo): Consumer {
+    if (typeof ci.config.deliver_subject === "string") {
+      throw new Error("not a pull consumer");
+    }
+    return new PullConsumerImpl(this.api, ci);
   }
 
   async ordered(
